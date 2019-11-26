fb90d1f4075240887ee4a78ed7cb7daa086ea830
javiergutierrezp <javiergutzp@hotmail.com>
2019-11-25 18:30:43 -0600

    Add files via upload

---
+++ FRONTEND/js/todo.js
@@ -0,0 +1,78 @@
+var token = localStorage.getItem('token');
+if (token) {
+  token = token.replace(/^"(.*)"$/, '$1'); // Remove quotes from token start/end.
+}
+
+
+var todos = document.querySelectorAll("input[type=checkbox]");
+
+function loadTodos() {
+  $.ajax({
+    url: 'https://examenfinaljavi.herokuapp.com/todos',
+    headers: {
+        'Content-Type':'application/json',
+        'Authorization': 'Bearer ' + token
+    },
+    method: 'GET',
+    dataType: 'json',
+    success: function(data){
+      console.log(data)
+
+      for( let i = 0; i < data.length; i++) {
+        // aqui va su código para agregar los elementos de la lista
+        console.log(data[i].description)
+        // algo asi:
+        addTodo(data[i]._id, data[i].description, data[i].completed)
+      }
+    },
+    error: function(error_msg) {
+      alert((error_msg['responseText']));
+    }
+  });
+}
+
+loadTodos()
+
+
+// o con jquery
+// $('input[name=newitem]').keypress(function(event){
+//     var keycode = (event.keyCode ? event.keyCode : event.which);
+//     if(keycode == '13'){
+//         $.ajax({})
+//     }
+// });
+
+var input = document.querySelector("input[name=newitem]");
+
+input.addEventListener('keypress', function (event) {
+  if (event.charCode === 13) {
+    json_to_send = {
+      "description" : input.value
+    };
+    json_to_send = JSON.stringify(json_to_send);
+    $.ajax({
+      url: 'https://examenfinaljavi.herokuapp.com/todos',
+      headers: {
+          'Content-Type':'application/json',
+          'Authorization': 'Bearer ' + token
+      },
+      method: 'POST',
+      dataType: 'json',
+      data: json_to_send,
+      success: function(data){
+        console.log(data)
+        addTodo(data[i]._id, data[i].description, data[i].completed)
+        
+      },
+      error: function(error_msg) {
+        alert((error_msg['responseText']));
+      }
+    });
+    input.value = '';
+  }
+})
+
+
+function addTodo(id, todoText, completed) {
+$('#todo-list').append('<li><input type="checkbox" name="todo" value="'+id+'" checked><span>'+todoText+'</span></li>')
+}
